fc89d9987793510ada52c7a22143846c961f6dee
Pacito2 <ian.ledigfr@gmail.com>
2020-05-08 21:34:13 +0200

    Invitation request update

--- src/Manager/ClientHandler.java
+++ src/Manager/ClientHandler.java
@@ -30,42 +30,63 @@ public class ClientHandler extends Thread
                     String[] args = clientMessage.substring(1).split("\\s+");
                     System.out.println(client.getName() + " : " + clientMessage);
                     switch (args[0]){
-                        case "leave" :
+                        case "commands" :
+                            client.getWriter().writeUTF("Command list : quit, leave, party");
+                            break;
+                        case "quit" :
                             PartyHandler.sendMessageToParty(client.getParty(), client.getName() + " left the chat");
                             client.createParty();
-                            System.out.println(client.getName() + " leave : " + client.getSocket());
+                            System.out.println(client.getName() + " disconnected : " + client.getSocket());
                             client.getSocket().close();
                             return;
-                        case "invite" :
-                            if(args.length == 2) {
-                                // Try to invite a client
-                                String responseMessage = PartyHandler.tryInviteParty(client, args[1]);
-                                client.getWriter().writeUTF(responseMessage);
-                                System.out.println("[" + client.getName() + " " + args[0] + "] : " + responseMessage);
+                        case "party" :
+                            if(args.length > 1){
+                                switch (args[1]){
+                                    case "invite" :
+                                        if(args.length == 3) {
+                                            // Try to invite a client
+                                            String responseMessage = PartyHandler.tryInviteParty(client, args[2]);
+                                            client.getWriter().writeUTF(responseMessage);
+                                            System.out.println("[" + client.getName() + " " + args[0] + "] : " + responseMessage);
+                                        }
+                                        else
+                                            client.getWriter().writeUTF("Invalid syntax : /" + args[0] + " " + args[1] + " <username>");
+                                        break;
+                                    case "join" :
+                                        if(args.length == 3) {
+                                            // Try to join a client
+                                            String responseMessage = PartyHandler.tryJoinUser(client, args[2]);
+                                            client.getWriter().writeUTF(responseMessage);
+                                            System.out.println("[" + client.getName() + " " + args[0] + " " + args[1] + "] : " + responseMessage);
+                                        }
+                                        else
+                                            client.getWriter().writeUTF("Invalid syntax : /join <username>");
+                                        break;
+                                    case "list":
+                                        String users = "";
+                                        int number = 0;
+                                        for(User user : client.getParty().getUserList()){
+                                            users += user.getName() + ", ";
+                                            number++;
+                                        }
+                                        users = users.substring(0, users.length() - 2);
+                                        System.out.println("[" + client.getName() + " " + args[0] + args[1] + "] : " + users);
+                                        client.getWriter().writeUTF(number + " party member(s) : " + users);
+                                        break;
+                                }
+                                break;
                             }
                             else
-                                client.getWriter().writeUTF("Invalid syntax : /" + args[1] + " <username>");
+                                client.getWriter().writeUTF("Invalid syntax : /party <invite/join/list>");
                             break;
-                        case "join" :
-                            if(args.length == 2) {
-                                // Try to join a client
-                                String responseMessage = PartyHandler.tryJoinUser(client, args[1]);
-                                client.getWriter().writeUTF(responseMessage);
-                                System.out.println("[" + client.getName() + " " + args[0] + "] : " + responseMessage);
+                        case "leave" :
+                            if(client.getParty().getUserList().size() > 1){
+                                PartyHandler.sendMessageToParty(client.getParty(), client.getName() + " left the chat!");
+                                client.createParty();
+                                System.out.println(client.getName() + " left his chat");
                             }
                             else
-                                client.getWriter().writeUTF("Invalid syntax : /accept <username>");
-                            break;
-                        case "party":
-                            String users = "";
-                            int number = 0;
-                            for(User user : client.getParty().getUserList()){
-                                users += user.getName() + ", ";
-                                number++;
-                            }
-                            users = users.substring(0, users.length() - 2);
-                            System.out.println("[" + client.getName() + " " + args[0] + "] : " + users);
-                            client.getWriter().writeUTF(number + " party member(s) : " + users);
+                                client.getWriter().writeUTF("You're already alone!");
                             break;
                     }
                 }
